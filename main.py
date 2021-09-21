ae8bad6e542ae5a6c138f6682a5d8739351935cf
Terry Geng <gengyd@umd.edu>
2021-09-21 18:22:24 -0400

    refactor(main): Moved MainWindow into separate file

--- main.py
+++ main.py
@@ -1,61 +1,17 @@
 # This Python file uses the following encoding: utf-8
 import sys
 import os
-import time
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QScrollArea
-from PyQt5.QtCore import Qt, QCoreApplication, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtCore import Qt, QCoreApplication
 
 from wavemeter_dashboard.controller.alert_tracker import AlertTracker
-from wavemeter_dashboard.model.channel_model import ChannelModel
-from wavemeter_dashboard.view.dashboard import Dashboard
 from wavemeter_dashboard.controller.monitor import Monitor
 from wavemeter_dashboard.controller.wavemeter_ws7 import WavemeterWS7
 from wavemeter_dashboard.controller.fiber_switch import FiberSwitch
 from wavemeter_dashboard.controller.arduino_dac import DAC
 from wavemeter_dashboard.util import solve_filepath
 from wavemeter_dashboard import config
-from wavemeter_dashboard.view.single_channel_display import SingleChannelDisplay
-
-
-class MainWindow(QMainWindow):
-    def __init__(self, monitor: Monitor, alert_tracker: AlertTracker):
-        QMainWindow.__init__(self)
-
-        self.monitor = monitor
-        self.alert_tracker = alert_tracker
-
-        self.scroll = QScrollArea(self)
-        self.scroll.setWidgetResizable(True)
-        self.setCentralWidget(self.scroll)
-
-        self.dashboard = None
-        self.single_channel_display = None
-
-        self.channels = []
-        channel_configs = config.config.get("channels", [])
-        for chan in channel_configs:
-            self.channels.append(ChannelModel.from_settings_dict(chan))
-
-    def switch_to_dashboard(self):
-        self.dashboard = Dashboard(self, monitor, alert_tracker, self.channels)
-        self.dashboard.on_switch_to_single_channel_display_clicked.connect(
-            self.switch_to_single_channel_display)
-
-        self.scroll.setWidget(self.dashboard)
-
-        if self.single_channel_display:
-            self.single_channel_display = None
-
-    def switch_to_single_channel_display(self, channel):
-        self.single_channel_display = SingleChannelDisplay(
-            self, monitor, alert_tracker, channel)
-        self.single_channel_display.on_switch_to_dashboard_clicked.connect(
-            self.switch_to_dashboard)
-        self.scroll.setWidget(self.single_channel_display)
-
-        if self.dashboard:
-            self.dashboard = None
-
+from wavemeter_dashboard.view.main_window import MainWindow
 
 if __name__ == "__main__":
     QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
@@ -92,11 +48,7 @@ if __name__ == "__main__":
     with open(style) as f:
         window.setStyleSheet(f.read())
 
-    if config.config.get("full_screen", False):
-        window.showFullScreen()
-    else:
-        window.show()
-        window.resize(1400, 700)
+    window.show()
 
     window.switch_to_dashboard()
 
---
+++ wavemeter_dashboard/view/main_window.py
@@ -0,0 +1,55 @@
+from PyQt5.QtWidgets import QMainWindow, QScrollArea
+
+from wavemeter_dashboard.config import config
+from wavemeter_dashboard.controller.alert_tracker import AlertTracker
+from wavemeter_dashboard.controller.monitor import Monitor
+from wavemeter_dashboard.model.channel_model import ChannelModel
+from wavemeter_dashboard.view.dashboard import Dashboard
+from wavemeter_dashboard.view.single_channel_display import SingleChannelDisplay
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, monitor: Monitor, alert_tracker: AlertTracker):
+        QMainWindow.__init__(self)
+
+        self.monitor = monitor
+        self.alert_tracker = alert_tracker
+
+        self.scroll = QScrollArea(self)
+        self.scroll.setWidgetResizable(True)
+        self.setCentralWidget(self.scroll)
+
+        self.dashboard = None
+        self.single_channel_display = None
+
+        self.channels = []
+        channel_configs = config.get("channels", [])
+        for chan in channel_configs:
+            self.channels.append(ChannelModel.from_settings_dict(chan))
+
+    def switch_to_dashboard(self):
+        self.dashboard = Dashboard(self, self.monitor, self.alert_tracker, self.channels)
+        self.dashboard.on_switch_to_single_channel_display_clicked.connect(
+            self.switch_to_single_channel_display)
+
+        self.scroll.setWidget(self.dashboard)
+
+        if self.single_channel_display:
+            self.single_channel_display = None
+
+    def switch_to_single_channel_display(self, channel):
+        self.single_channel_display = SingleChannelDisplay(
+            self, self.monitor, self.alert_tracker, channel)
+        self.single_channel_display.on_switch_to_dashboard_clicked.connect(
+            self.switch_to_dashboard)
+        self.scroll.setWidget(self.single_channel_display)
+
+        if self.dashboard:
+            self.dashboard = None
+
+    def show(self):
+        if config.get("full_screen", False):
+            super().showFullScreen()
+        else:
+            super().show()
+            self.resize(1400, 700)
